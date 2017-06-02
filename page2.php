3f4a81fb430a895c6975400201a550ab6dc015e9
rodmahe <rodmahe@gmail.com>
2017-06-02 11:22:23 +0200

    rajout du fichier page2.php et modif de uploadFichier.php

--- page2.php
+++ page2.php
@@ -13,10 +13,7 @@ rajouter enctype="multipart/form-data"
 method="POST" obligatoire
 -->
       
-<form method="post" action="traitement.php" enctype="multipart/form-data">
-    <input type="file" name="fichier">
-    <button type="submit">Envoyer</button>
-</form>
+
        
        
 </body>
